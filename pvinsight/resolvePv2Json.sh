b60102a73263bf7cb7aa14625d6506f30c17233c
Loong <dragonjia@gmail.com>
2018-08-13 15:38:27 +0800

    update all-in-one

--- pvinsight/resolvePv2Json.sh
+++ pvinsight/resolvePv2Json.sh
@@ -1,4 +1,40 @@
 #!/usr/bin/env bash
 
+file='/usr/local/nginx/logs/pv_access_log'
+lastTime=`cat last.log`
+if [[ -z $lastTime ]];
+then
+    lastTime=0
+fi
 
-tail -100f ../logs/pv_access.log |awk '{ip=$1;split($7,m,"=");for(i=1;i<10;i+=2){if(index(m[i],"pvInsightObj")>0){print m[i+1]}}}'
+echo $lastTime
+
+tail -100f ../logs/pv_access.log |awk -v lastTime=$lastTime '
+function urlDecode(url) {
+    for (i = 0x20; i < 0x40; ++i) {
+        repl = sprintf("%c", i);
+        if ((repl == "&") || (repl == "\\")) {
+            repl = "\\" repl;
+        }
+        url = gensub(sprintf("%%%02X", i), repl, "g", url);
+        url = gensub(sprintf("%%%02x", i), repl, "g", url);
+    }
+    return url;
+}
+
+BEGIN{
+    #FS="\""
+}
+{
+time=$3;
+ip=$1;
+if(time>lastTime){
+    split($7,m,"=");
+    for(i=1;i<10;i+=2){
+        if(index(m[i],"pvInsightObj")>0){
+            print urlDecode(m[i+1])
+        }
+    }
+}
+}END{
+}'
